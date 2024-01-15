439505102b85bdc3d2af5e9bed226c38c14e3f75
Federico Di Franco <thepiovrus14@gmail.com>
2024-01-15 19:32:04 +0100

    Added change theme for icons

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,4 +1,16 @@
 function myFunction() {
     var element = document.body;
     element.dataset.bsTheme = element.dataset.bsTheme == "dark" ? "light" : "dark";
+
+    var icons = document.querySelectorAll('.theme-icon');
+    icons.forEach(function(icon) {
+        if (element.dataset.bsTheme == "dark") {
+            icon.classList.remove('text-dark');
+            icon.classList.add('text-light');
+        } else {
+            icon.classList.remove('text-light');
+            icon.classList.add('text-dark');
+        }
+    });
 }
+
