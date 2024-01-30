bfd1bf4ea0e4fa4d1b2d46858ebcb36efc770e1a
Mattia Forti <mattia.forti2@studio.unibo.it>
2024-01-30 18:04:09 +0100

    [MOD] Added numbers for followers, following and posts

--- controller/profile.js
+++ controller/profile.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function () {
-
     let user = sessionStorage.getItem("userEmail");
+
+    // Dati dell'utente
     $.ajax({
         url: '../../model/getUserInfo.php',
         type: 'POST',
@@ -8,16 +9,23 @@ window.addEventListener("load", function () {
             targetUser: user,
         },
         dataType: 'json',
-        success: function (data) {
-            let userData = data[0];
-            $("#user-username").text(userData.Username);
-            $("#user-name").text(userData.Nome + " " + userData.Cognome);
-            $("#user-bio").text(userData.Descrizione);
-            $("#user-birth").text(" Nato il : " + userData.DataNascita);
-            if (userData.FotoProfilo) {
-                $("#user-photo").attr("src", "data:image/jpeg;base64," + userData.FotoProfilo);
+        success: function (response) {
+            if (response.success) {
+                let userData = response.userData[0];
+                $("#user-username").text(userData.Username);
+                $("#user-name").text(userData.Nome + " " + userData.Cognome);
+                $("#user-bio").text(userData.Descrizione);
+                $("#user-birth").text(" Nato il : " + userData.DataNascita);
+                if (userData.FotoProfilo) {
+                    $("#user-photo").attr("src", "data:image/jpeg;base64," + userData.FotoProfilo);
+                } else {
+                    $("#user-photo").attr("src", "../../resources/img/place-holder-pfp.jpg");
+                }
+                document.getElementById("seguiti").innerHTML = response.followingCount;
+                document.getElementById("followers").innerHTML = response.followersCount;
+                document.getElementById("post").innerHTML = response.postCount;
             } else {
-                $("#user-photo").attr("src", "../../resources/img/place-holder-pfp.jpg");
+                console.log(response.error);
             }
         },
         error: function (error) {
--- model/getUserInfo.php
+++ model/getUserInfo.php
@@ -3,29 +3,44 @@
 require_once("../model/connection/dbConnection.php");
 include("../model/login/loginUtilities.php");
 
-$query = "SELECT * FROM utenti WHERE Email = ? LIMIT 1"; 
+function getCount($conn, $query) {
+    $count = 0;
+    if ($stmt = $conn->prepare($query)) {
+        $stmt->bind_param("s", $_SESSION['userEmail']); 
+        if ($stmt->execute()) {
+            $stmt->bind_result($count);
+            $stmt->fetch();
+        }
+    }
+    return $count;
+}
 
-if ($selectQuery = $conn->prepare($query)) {
-    $selectQuery->bind_param("s", $_POST['targetUser']); 
-    if ($selectQuery->execute()) {
-        $results = $selectQuery->get_result();
+$infoQuery = "SELECT * FROM utenti WHERE Email = ? LIMIT 1"; 
+
+session_start();
+if ($stmt = $conn->prepare($infoQuery)) {
+    $stmt->bind_param("s", $_SESSION['userEmail']);
+    if ($stmt->execute()) {
+        $results = $stmt->get_result();
         if ($results->num_rows > 0) {
             $userData = $results->fetch_all(MYSQLI_ASSOC);
             $userData[0]['FotoProfilo'] = base64_encode($userData[0]['FotoProfilo']);
-            echo json_encode($userData);
+            $response = array("success" => true, 
+                "userData" => $userData,
+                "postCount" => getCount($conn, "SELECT COUNT(*) FROM post WHERE EmailUtente = ?"),
+                "followersCount" => getCount($conn, "SELECT COUNT(*) FROM follow WHERE EmailUtenteSeguito = ?"),
+                "followingCount" => getCount($conn, "SELECT COUNT(*) FROM follow WHERE EmailUtente = ?"));
         } else {
-            $e = array("error" => "User not found.");
-            echo json_encode($e);
+            $response = array("success" => false, "error" => "Utente non trovato");
         }
     } else {
-        $e = array("error" => $selectQuery->error);
-        echo json_encode($e);
+        $response = array("success" => false, "error" => $stmt->error);
     }
 } else {
-    $e = array("error" => $conn->error);
-    echo json_encode($e);
+    $response = array("success" => false, "error" => $conn->error);
 }
 
+echo json_encode($response);
 $conn->close();
 
 ?>
